a6ba9b97b24c8704d2a1f1fcdbbe4e7d5dd5b492
Ilya Smirnov <smilya13@gmail.com>
2020-02-07 14:15:25 +0300

    feat: add consultation requests logging

--- php/consultation.php
+++ php/consultation.php
@@ -10,4 +10,11 @@ $mailBody = mb_convert_encoding($finalStr, "windows-1251", "auto");
 
 mail('smilya@yandex.ru', "Site: Новая запись на персональную консультацию", $mailBody);
 
+$date = date ("Y-m-d H:i:s");
+$strForLog = $date."\r\n".$secondSrt.$thirdStr.$fourthStr."\r\n\r\n";
+
+$file = fopen("../data/consultation.log", "a");
+fputs($file, $strForLog);
+fclose($file); 
+
 ?>
