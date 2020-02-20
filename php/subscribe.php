521e53eb31f33a4c9ead604f1db95293a97ca068
Ilya Smirnov <smilya13@gmail.com>
2020-02-20 14:42:58 +0300

    feat: add faq page content

--- php/subscribe.php
+++ php/subscribe.php
@@ -7,10 +7,11 @@ $ifSuccess = file_put_contents("../data/subscription.dat", $allNewMails);
 
 $date = date ("Y-m-d H:i:s");
 $fromLog = file_get_contents("../data/subscription.log");
-$toLog = $fromLog.$date.": ".$newMail."\r\n";
+$toMail = $date.": ".$newMail."\r\n";
+$toLog = $fromLog.$toMail;
 $ifLogSuccess = file_put_contents("../data/subscription.log", $toLog);
 
-mail('smilya@yandex.ru', "Site: Новая подписка на рассылку", $toLog);
+mail('smilya@yandex.ru', "Site: Новая подписка на рассылку", $toMail);
 
 echo ($ifSuccess && $ifLogSuccess);
 
