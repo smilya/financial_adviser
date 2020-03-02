6b50df0910c33d2d95a766da3d0ba4f2cb22684f
Ilya Smirnov <smilya13@gmail.com>
2020-03-02 05:33:50 +0300

    feat: add js to service-opinion page

---
+++ data/service-opinion-data.js
@@ -0,0 +1,20 @@
+let cases = [
+  {
+    title: "Кейс 1",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam ipsum voluptatibus inventore expedita possimus excepturi voluptate totam, assumenda illo unde soluta quae facere sapiente iusto, eos sunt atque quo sit. Officia, placeat ipsam. Dicta nemo provident hic dolore libero eos sed eum at. Ipsum obcaecati, nulla enim dolorum id repudiandae cum veritatis cumque modi labore!",
+    analytics: "",
+    report: "",
+  },
+  {
+    title: "Кейс 2",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam ipsum voluptatibus inventore expedita possimus excepturi voluptate totam, assumenda illo unde soluta quae facere sapiente iusto, eos sunt atque quo sit. Officia, placeat ipsam. Dicta nemo provident hic dolore libero eos sed eum at. Ipsum obcaecati, nulla enim dolorum id repudiandae cum veritatis cumque modi labore!",
+    analytics: "",
+    report: "",
+  },
+  {
+    title: "Кейс 3",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam ipsum voluptatibus inventore expedita possimus excepturi voluptate totam, assumenda illo unde soluta quae facere sapiente iusto, eos sunt atque quo sit. Officia, placeat ipsam. Dicta nemo provident hic dolore libero eos sed eum at. Ipsum obcaecati, nulla enim dolorum id repudiandae cum veritatis cumque modi labore!",
+    analytics: "",
+    report: "",
+  },
+];
---
+++ js/service-opinion.js
@@ -0,0 +1,45 @@
+let arrowsList = document.querySelectorAll('.consult-stages__arrows');
+let additionsList = document.querySelectorAll('.consult-stages-addition');
+
+for (let i = 0; i < arrowsList.length; i++) {
+  arrowsList[i].onclick = function() {
+    arrowsList[i].querySelector('.service-arrow-whenOpen').classList.toggle('hidden');
+    arrowsList[i].querySelector('.service-arrow-whenClosed').classList.toggle('hidden');
+    additionsList[i].classList.toggle('hidden');
+  }
+}
+
+let pagination = document.querySelector('.slider-pagination');
+for (let i = 0; i < cases.length; i++) {
+  let dot = document.createElement('div');
+  dot.classList.add('slider-pagination-dot');
+  pagination.append(dot);
+}
+
+let caseNumber = 0;
+fillSlider(caseNumber);
+
+function fillSlider(num) {
+  let dots = document.querySelectorAll('.slider-pagination-dot');
+  for (let dot of dots) {
+    dot.classList.remove('active');
+  }
+  let blackout = document.querySelector('.slider-blackout');
+  blackout.classList.remove('hidden');
+  setTimeout(() => blackout.classList.add('hidden'), 100);
+  document.querySelector('.slider-service-opinion__container h4').innerText = cases[num].title;
+  document.querySelector('.slider-service-opinion__container p').innerText = cases[num].text;
+  dots[num].classList.add('active');
+}
+
+document.querySelector('.slider-button--right').onclick = function() {
+  if(caseNumber < cases.length - 1) { caseNumber++ }
+  else { caseNumber = 0; }
+  fillSlider(caseNumber);
+}
+
+document.querySelector('.slider-button--left').onclick = function() {
+  if(caseNumber > 0) { caseNumber-- }
+  else { caseNumber = cases.length - 1; }
+  fillSlider(caseNumber);
+}
