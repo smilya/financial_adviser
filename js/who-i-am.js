ef9dd08f721a7c6e12edc54d79b1f895efdd1a35
Ilya Smirnov <smilya13@gmail.com>
2020-03-04 13:23:50 +0300

    fix: correc documents show at about-me page

--- js/who-i-am.js
+++ js/who-i-am.js
@@ -45,27 +45,26 @@ function showDocument(innerHTML) {
   let modal = document.createElement('div');
   modal.innerHTML = innerHTML;
   document.querySelector('.who-i-am__certificates').prepend(modal);
-  document.getElementById("blur-over").onclick = function() {modal.remove();};
-  document.querySelector(".modal-window").onclick = function() {modal.remove();};
+  document.querySelector('.container-forAction').onclick = function() {modal.remove();};
 }
 
 document.getElementById('showDiploma').onclick = function() {
-  inner = '<div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/diploma.jpg" alt="document"></div>';
+  inner = '<div class="container-forAction"><div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/diploma.jpg" alt="document"></div></div>';
   showDocument(inner);
 }
 
 document.getElementById('showREU').onclick = function() {
-  let inner = '<div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/diploma-REU.jpg" alt="document"></div>';
+  let inner = '<div class="container-forAction"><div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/diploma-REU.jpg" alt="document"></div></div>';
   showDocument(inner);
 }
 
 document.getElementById('showFSFR_1').onclick = function() {
-  let inner = '<div id="blur-over"></div><div class="modal-window modal-window--documents modal-window--verticalDoc"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/FSFR-1.jpg" alt="document"></div>';
+  let inner = '<div class="container-forAction"><div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/FSFR-1.jpg" alt="document"></div></div>';
   showDocument(inner);
 }
 
 document.getElementById('showFSFR_5').onclick = function() {
-  let inner = '<div id="blur-over"></div><div class="modal-window modal-window--documents modal-window--verticalDoc"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/FSFR-5.jpg" alt="document"></div>';
+  let inner = '<div class="container-forAction"><div id="blur-over"></div><div class="modal-window modal-window--documents"><div class="close-cross close-cross-documents">×</div><img class="modal-window-image" src="../images/who-i-am/FSFR-5.jpg" alt="document"></div></div>';
   showDocument(inner);
 }
 
